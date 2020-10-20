31354d5e0f02c5057f492c2b02400480e93da2ca
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-10-20 16:24:52 -0400

    posting a user

---
+++ src/UserForm.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+export default class UserForm extends React.Component {
+  state = {
+    username: "",
+    password: "",
+    email: "",
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        accepts: "application/json",
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(this.state),
+    })
+      .then((response) => response.json())
+      .then((response) => console.log(response));
+  };
+  
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          name="username"
+          placeholder="Enter Username"
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter Password"
+          value={this.state.password}
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          name="email"
+          placeholder="Enter Email"
+          value={this.state.email}
+          onChange={this.handleChange}
+        />
+        <input type="submit" value="Submit"></input>
+      </form>
+    );
+  }
+}
