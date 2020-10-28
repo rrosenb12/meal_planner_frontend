09973af3f5c89270d63fa28439a01c31b37ab5e4
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-10-28 17:28:53 -0400

    basic routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
-import UserForm from "./UserForm";
+import UserForm from "./Components/UserForm";
+import NavBar from "./Components/NavBar"
+import {BrowserRouter, Route} from "react-router-dom"
 import "./App.css";
 
-class App extends React.Component {
+export default class App extends React.Component {
   state = {
     user: null,
   };
@@ -23,47 +25,49 @@ class App extends React.Component {
 
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
-        <UserForm />
-        <div
-          class="btn-toolbar"
-          role="toolbar"
-          aria-label="Toolbar with button groups"
-        >
-          <div class="btn-group mr-2" role="group" aria-label="First group">
-            <button type="button" class="btn btn-secondary">
-              1
-            </button>
-            <button type="button" class="btn btn-secondary">
-              2
-            </button>
-            <button type="button" class="btn btn-secondary">
-              3
-            </button>
-            <button type="button" class="btn btn-secondary">
-              4
-            </button>
-          </div>
-          <div class="btn-group mr-2" role="group" aria-label="Second group">
-            <button type="button" class="btn btn-secondary">
-              5
-            </button>
-            <button type="button" class="btn btn-secondary">
-              6
-            </button>
-            <button type="button" class="btn btn-secondary">
-              7
-            </button>
-          </div>
-          <div class="btn-group" role="group" aria-label="Third group">
-            <button type="button" class="btn btn-secondary">
-              8
-            </button>
-          </div>
-        </div>
+        <Route path="/" component={NavBar}/>
+        <Route exact path="/login-signup" component={UserForm} />
       </div>
+      </BrowserRouter>
     );
   }
 }
 
-export default App;
+{/* <div
+  class="btn-toolbar"
+  role="toolbar"
+  aria-label="Toolbar with button groups"
+>
+  <div class="btn-group mr-2" role="group" aria-label="First group">
+    <button type="button" class="btn btn-secondary">
+      1
+    </button>
+    <button type="button" class="btn btn-secondary">
+      2
+    </button>
+    <button type="button" class="btn btn-secondary">
+      3
+    </button>
+    <button type="button" class="btn btn-secondary">
+      4
+    </button>
+  </div>
+  <div class="btn-group mr-2" role="group" aria-label="Second group">
+    <button type="button" class="btn btn-secondary">
+      5
+    </button>
+    <button type="button" class="btn btn-secondary">
+      6
+    </button>
+    <button type="button" class="btn btn-secondary">
+      7
+    </button>
+  </div>
+  <div class="btn-group" role="group" aria-label="Third group">
+    <button type="button" class="btn btn-secondary">
+      8
+    </button>
+  </div>
+</div> */}
---
+++ src/Components/NavBar.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function NavBar(props){
+    return(
+        <h1>hi</h1>
+    )
+}
