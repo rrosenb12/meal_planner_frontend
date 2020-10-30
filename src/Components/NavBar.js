74bc43a8252962410fc788714b88105dc9b4e111
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-10-30 00:38:19 -0400

    navbar styling

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function NavBar(props){
     return(
         <div className="navbar">
-            <h1 className="navbar-title">mealzy</h1>
+            <title-right >mealzy</title-right>
         </div>
     )
 }
