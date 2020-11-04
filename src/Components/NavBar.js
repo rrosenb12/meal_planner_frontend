804585f136b95f1abc2711d20d3c395dde1da66f
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-11-04 12:30:27 -0500

    hiding scrolling bar and animating sidebar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,27 +1,50 @@
-import React, {useState, useCallback} from 'react'
-import closedSidebar from "../Images/closed-sidebar.png"
+import React, { useState, useCallback } from "react";
+import { NavLink } from "react-router-dom";
+import closedSidebar from "../Images/closed-sidebar.png";
+import openedSidebar from "../Images/opened-sidebar.png";
 
-export default function NavBar(){
+export default function NavBar() {
+  const [isClicked, setIsClicked] = useState(false);
+  const click = useCallback(() => setIsClicked(!isClicked), [
+    isClicked,
+    setIsClicked,
+  ]);
 
-    const [isClicked, setIsClicked] = useState(false)
-    const click = useCallback(() => setIsClicked(!isClicked), [isClicked, setIsClicked])
-
-    return(
-        <div className="navbar">
-            
-            <side-menu>
-                <img src={closedSidebar} alt="the sidebar menu is closed" onClick={click}/>
-                <p>is clicked? {String(isClicked)}</p>
-                <ul className="sidebar-options" >
-                    <li>
-                        Login
-                    </li>
-                    <li>
-                        Signup
-                    </li>
-                </ul>
-            </side-menu>
-            <title-right>mealzy</title-right>
-        </div>
-    )
+  return (
+    <>
+      {isClicked ? (
+        <side-menu style={{ width: "25%" }}>
+          <ul className="sidebar-options">
+            <NavLink to="/login">
+              <li>Login</li>
+            </NavLink>
+            <NavLink to="/signup">
+              <li>Signup</li>
+            </NavLink>
+          </ul>
+        </side-menu>
+      ) : null}
+      <div
+        style={isClicked ? { "margin-left": "25%" } : { "margin-left": "0" }}
+        className="navbar"
+      >
+        <image-wrapper>
+          {isClicked ? (
+            <img
+              src={openedSidebar}
+              alt="the sidebar menu is opened"
+              onClick={click}
+            />
+          ) : (
+            <img
+              src={closedSidebar}
+              alt="the sidebar menu is closed"
+              onClick={click}
+            />
+          )}
+        </image-wrapper>
+        <title-right>mealzy</title-right>
+      </div>
+    </>
+  );
 }
